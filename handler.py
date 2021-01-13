de73c939764a3e609c9aa3bfdb64e9d5a7263182
young.tsai <young.tsai@commerce-connector.com>
2021-01-13 09:47:23 +0800

    action try

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import json
 
 def get_joke(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless v1.0! Your function executed successfully!  try push Action!",
     }
     response = {
         "statusCode": 200,
