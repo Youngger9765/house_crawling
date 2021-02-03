28f773cc59ecff61b61d2977ce19707816e528b1
young.tsai <young.tsai@commerce-connector.com>
2021-02-03 15:35:06 +0800

    clean

--- util/crawler.py
+++ util/crawler.py
@@ -34,8 +34,6 @@ class lejuCrawler:
 
         self.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
-       
-
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--incognito')
         chrome_options.add_argument('headless')
