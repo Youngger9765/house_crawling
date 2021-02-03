94772f452884ade7f412ed8b9e369174ff13b85b
young.tsai <young.tsai@commerce-connector.com>
2021-02-03 15:36:32 +0800

    url_list

--- leju.py
+++ leju.py
@@ -14,21 +14,21 @@ def get_data():
     sht_worker = gsheet_worker()
     url_list = sht_worker.get_col_all_value("bot-list", 2)[1:]
     print(url_list)
-    # url_list = [
-    #     "https://www.leju.com.tw/page_search_result?oid=L37611690f7027", #麗軒珍寶
-    #     "https://www.leju.com.tw/page_search_result?oid=L93811608b8266", #世紀公園B
-    # ]
+    url_list = [
+        "https://www.leju.com.tw/page_search_result?oid=L37611690f7027", #麗軒珍寶
+        "https://www.leju.com.tw/page_search_result?oid=L93811608b8266", #世紀公園B
+    ]
     body = {'profile':[]}
     for url in url_list:
-        # try:
-        leju_crawler = lejuCrawler()
-        data = leju_crawler.fetch_data(url)
-        data_json = leju_crawler.get_data_json(data)
-        data_json = json.dumps(data_json, ensure_ascii=False).encode('utf8')
-        body['profile'].append(data_json.decode())
-        # except Exception as e:
-        #     print(f"fetch fail:{url}")
-        #     print(repr(e))
+        try:
+            leju_crawler = lejuCrawler()
+            data = leju_crawler.fetch_data(url)
+            data_json = leju_crawler.get_data_json(data)
+            data_json = json.dumps(data_json, ensure_ascii=False).encode('utf8')
+            body['profile'].append(data_json.decode())
+        except Exception as e:
+            print(f"fetch fail:{url}")
+            print(repr(e))
             
 
 
