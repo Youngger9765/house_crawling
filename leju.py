17a1d3e2d984550f6f4f312745ea6a7486792d42
Tzu Yang <purpleice9765@msn.com>
2021-03-26 10:48:06 +0800

    refactor

--- leju.py
+++ leju.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
-from util.gsheet import gsheet_worker 
+from util.gsheet import gsheet_worker, leju_gsheet_worker
 from util.crawler import lejuCrawler 
 import json
 
+# Params
+def web_config(name):
+    config = {
+        "leju":{
+            "url_list_tab": "bot-list",
+            "crawler": lejuCrawler(),
+        }
+    }
+    config_data = config[name]
 
-def crawl(event, context):
-    data = get_data()
-    write_to_sheet(data)
+    return config_data
 
-def get_data():
-    # crawl
+
+# Main function
+def crawl(web_name):
+    data = get_data(web_name)
+    write_to_sheet(data,web_name)
+
+def get_data(web_name):
+    # config
+    config_data = web_config(web_name)
+    tab_name = config_data['url_list_tab']
+
+    # get by sheet
     sht_worker = gsheet_worker()
-    url_list = sht_worker.get_col_all_value("bot-list", 2)[1:]
-    print(url_list)
-    # url_list = [
+    # url_list = sht_worker.get_col_all_value(tab_name, 2)[1:]
+    # print(url_list)
+    
+    url_list = [
         # "https://www.leju.com.tw/page_search_result?oid=L37611690f7027", #麗軒珍寶
         # "https://www.leju.com.tw/page_search_result?oid=L93811608b8266", #世紀公園B
-    #     "https://www.leju.com.tw/page_search_result?oid=L08243922184a6", #公園大鎮
-    # ]
+        # "https://www.leju.com.tw/page_search_result?oid=L08243922184a6", #公園大鎮
+        "https://www.leju.com.tw/page_search_result?oid=Lf2b86691c9621"
+    ]
+    
+    # to json file
     body = {'profile':[]}
     for url in url_list:
         try:
-            leju_crawler = lejuCrawler()
-            data = leju_crawler.fetch_data(url)
-            data_json = leju_crawler.get_data_json(data)
+            crawler = config_data['crawler']
+            data = crawler.fetch_data(url)
+            data_json = crawler.get_data_json(data)
             data_json = json.dumps(data_json, ensure_ascii=False).encode('utf8')
             body['profile'].append(data_json.decode())
         except Exception as e:
@@ -35,12 +56,12 @@ def get_data():
     return body
 
 
-def write_to_sheet(data):
-    sht_worker = gsheet_worker()
+def write_to_sheet(data, web_name):
+    sht_worker = gsheet_worker(web_name)
     sht_worker.write_profile_to_sheet(data)
 
 if __name__ == "__main__":
-    crawl("","")
+    crawl("leju")
 
     # url = "https://www.leju.com.tw/page_search_result?oid=L08243922184a6"
     # leju_crawler = lejuCrawler()
--- util/gsheet.py
+++ util/gsheet.py
@@ -11,8 +11,12 @@ import requests
 from datetime import datetime as dt
 
 class gsheet_worker:
-    def __init__(self):
+    def __init__(self,web_name=None):
         self.sheet_key = '15V1XD3p_mD8SSP_TQkY2PwYTM_FjOAXQXD1GuJcrpfI'
+        if web_name == "leju":
+            self.sheet_worker = leju_gsheet_worker()
+        else:
+            self.sheet_worker = None
 
     def get_hash_str(self,data):
         hash_str = int(hashlib.sha1(data.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
@@ -27,6 +31,53 @@ class gsheet_worker:
         sheet = gss_client.open_by_key(spreadsheet_key_path)
         return sheet
     
+    def data_to_sheet_value_list(self, data):
+        sheet_worker = self.sheet_worker
+        sheet_value_list = sheet_worker.data_to_sheet_value_list(data)
+
+        return sheet_value_list
+
+    def get_col_all_value(self, tab_name, colunm_index):
+        sheet = self.get_sheet(self.sheet_key)
+        sheet_bot_link = sheet.worksheet(tab_name)
+        col_all_value = sheet_bot_link.col_values(colunm_index)
+        return col_all_value
+        
+        
+    def write_profile_to_sheet(self, data):
+        sheet = self.get_sheet(self.sheet_key)
+        sheet_bot = sheet.worksheet('bot')
+
+        link_list = self.get_col_all_value("bot", 6)
+        print("====link_list====")
+        print(link_list)
+
+        sheet_worker = self.sheet_worker
+        sheet_worker.write_profile_to_sheet(data, sheet, sheet_bot, link_list)
+        
+        message_list = sheet_worker.get_message_list()
+        for message in message_list:
+            self.send_line_notify(message)
+
+
+    def send_line_notify(self, message):
+        line_notify_token = "R7iIcVlcM4rBs0srfLtpea8bFrGhav3wBkX6V06of25"
+        url = "https://notify-api.line.me/api/notify"
+
+        headers = {
+            'Authorization': f"Bearer {line_notify_token}",
+        }
+        data = {
+            "message": message,
+        }
+        # To send data form-encoded
+        response = requests.post(url, headers=headers, data=data)
+            
+            
+class leju_gsheet_worker:
+    def __init__(self):
+        self.message_list = []
+
     def data_to_sheet_value_list(self, data):
         profile = json.loads(data)
         title = profile['title']
@@ -66,21 +117,7 @@ class gsheet_worker:
             
         return sheet_value_list
 
-    def get_col_all_value(self, tab_name, colunm_index):
-        sheet = self.get_sheet(self.sheet_key)
-        sheet_bot_link = sheet.worksheet(tab_name)
-        col_all_value = sheet_bot_link.col_values(colunm_index)
-        return col_all_value
-        
-        
-    def write_profile_to_sheet(self, data):
-        sheet = self.get_sheet(self.sheet_key)
-        sheet_bot = sheet.worksheet('bot')
-
-        link_list = self.get_col_all_value("bot", 6)
-        print("====link_list====")
-        print(link_list)
-
+    def write_profile_to_sheet(self, data, sheet, sheet_bot, link_list):
         profile_list = data['profile']
         for profile in profile_list:
             sheet_value_list = self.data_to_sheet_value_list(profile)
@@ -99,22 +136,13 @@ class gsheet_worker:
                 else:
                     sheet_bot.insert_row(sheet_value, sheet_row_cnt) 
                     message = f"{sheet_value[0]} 有新物件 {sheet_value[4]}，{sheet_value[6]} ，詳情請點擊公告" 
-                    self.send_line_notify(message)                  
+                    print(message)
+                    # self.send_line_notify(message) 
+                    self.message_list.append(message)                 
                     sheet_row_cnt +=1
 
-    def send_line_notify(self, message):
-        line_notify_token = "R7iIcVlcM4rBs0srfLtpea8bFrGhav3wBkX6V06of25"
-        url = "https://notify-api.line.me/api/notify"
-
-        headers = {
-            'Authorization': f"Bearer {line_notify_token}",
-        }
-        data = {
-            "message": message,
-        }
-        # To send data form-encoded
-        response = requests.post(url, headers=headers, data=data)
-            
-            
-            
+    def get_message_list(self):
+        message_list = self.message_list
+        return message_list
+          
         
