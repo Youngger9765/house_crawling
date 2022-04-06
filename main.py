f86d902f39f774f76a0198c770c71cf300d692d5
Youngger9765 <purpleice9765@msn.com>
2022-04-07 01:59:23 +0800

    http

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def cawl_test():
                 'refid': '18'
                 }
             rs.proxies = {
-                'https': 'https://15.235.41.55:8990',
+                'http': 'http://15.235.41.55:8990',
             }
             
             resp = rs.post(groupurl, headers=headers, params=params, data=data)
