e2b4864fbdaa712e80e1fc33f615533d4ec5d381
Youngger9765 <purpleice9765@msn.com>
2022-06-27 12:28:10 +0800

    run_DMP_scheduler notify

--- dmp_scheduler_handler.py
+++ dmp_scheduler_handler.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 from util.dmp_worker import DMP_schedule_worker
+from util.notification import LineWorker
 
 def run_DMP_scheduler():
+    line_notify_token ＝"Fms2ZdnfAf2us7knLNEdfTpb0ABbLHhnDcRbmzz3DQC"
+    line_worker = LineWorker(line_notify_token)
+    # notify
+    message = f"開始今日 DMP 預約爬蟲"
+    line_worker.send_notification(message)
+
     worker = DMP_schedule_worker()
     schedule_datetime_list = worker.get_schedule_datetime_list()
     available_row_index_list = worker.get_alive_row_index_list(schedule_datetime_list)
